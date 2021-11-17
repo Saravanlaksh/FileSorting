bb441267fad119efc088e322bfcc09abbefe8824
Saravanlaksh <94282008+Saravanlaksh@users.noreply.github.com>
2021-11-17 13:02:30 +0530

    Add files via upload

---
+++ FileDemo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+
+public class FileDemo
+{
+    public FileDemo()
+    {
+
+    }
+
+ 
+    public void ReadFromFile(int[] numbers)
+    {
+        string path = "C:\\Users\\40659\\source\\repos\\FileSortingDemo\\FileSortingDemo\\numbers.txt";
+        StreamReader sr = new StreamReader(path);
+        for (int index = 0; index < numbers.Length;index++)
+        {
+            numbers[index] = Convert.ToInt32(sr.ReadLine());
+
+        }
+        //close file when done.
+        sr.Close();
+    }
+
+    public void WriteToFile(int[] numbers)
+    {
+        string path = "C:\\Users\\40659\\source\\repos\\FileSortingDemo\\FileSortingDemo\\sortednumbers.txt";
+        StreamWriter sw = new StreamWriter(path);
+        for(int counter = 0; counter < numbers.Length; counter++)
+        {
+            sw.WriteLine( numbers[counter] );
+        }
+        sw.Close();
+    }
+    public void BubbleSortArray(int[] numbers)
+    {
+        bool swap;
+        int temp;
+        do
+        {
+            swap = false;
+            for(int index=0;index < (numbers.Length - 1);index++)
+            {
+                if(numbers[index] > numbers[index+1])
+                {
+                    //swap
+                    temp = numbers[index];
+                    numbers[index] = numbers[index + 1];
+                    numbers[index + 1] = temp;
+                    swap = true;
+                };
+            }
+        } while (swap == true);
+    }
+    public void DisplayArray(int[] numbers)
+    {
+        for (int index = 0; index < numbers.Length; index++)
+        {
+            Console.Write("{0} ", numbers[index]);
+        }
+
+        Console.WriteLine();
+    }
+
+}
---
+++ FileDemoCheck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class FileDemoCheck
+{
+
+    public static void Main(string[] args)
+    {
+        int[] numbers = new int[10];
+        FileDemo obj = new FileDemo();
+
+        //Call the read from file method
+        obj.ReadFromFile( numbers );
+        //Display the unsorted values
+        Console.Write("unsorted: ");
+        obj.DisplayArray( numbers );
+
+        //sort the numbers and display them again
+        obj.BubbleSortArray( numbers );
+        Console.Write("sorted: ");
+        obj.DisplayArray( numbers );
+
+        //save your data
+        obj.WriteToFile( numbers);
+    }
+}
